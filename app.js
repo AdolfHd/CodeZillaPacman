a54df75e0c0c8dbaebd49f1cff4aa54faa4f0255
AdolfHd <143146173+AdolfHd@users.noreply.github.com>
2023-08-25 06:27:27 +0500

    Add files via upload
    upload of files direcly on browser

---
+++ app.js
@@ -0,0 +1,37 @@
+import {gameLevels} from "./js/level.js";
+import {setScrObj, drawScreen, drawLevel, setGameLevel, movePacman} from "./js/screen.js";
+import {getPos} from "./js/utility.js"
+
+let currntLevel = 0;
+let inGameLevel = [];
+let pacmanPos = [];
+
+let sObj = {
+    height: 500,
+    width: 600,
+    backgroundColor: "black",
+    canvasId: "canvas0",
+    dimension: 10
+}
+
+class Game {
+    constructor(){
+        setScrObj(sObj);
+        drawScreen();
+        setGameLevel([...gameLevels[currntLevel]]);
+        drawLevel();
+    }
+    play = () => {
+        inGameLevel = [...gameLevels[currntLevel]];
+        setGameLevel(inGameLevel);
+        pacmanPos = getPos(inGameLevel,5);
+
+        document.addEventListener("keydown", (e) => {
+            pacmanPos = movePacman(e.key, pacmanPos);
+        });
+    };
+
+}
+
+let pacmanGame = new Game();
+pacmanGame.play();
